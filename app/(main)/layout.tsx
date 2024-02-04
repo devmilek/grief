91259b365a2e282dc35871fdd7a67898c78b4bf4
Miłosz <devmilek@gmail.com>
2024-02-04 20:29:52 +0100

    fix: caching utility data

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,19 +3,17 @@ import Navbar from "@/components/navigation/navbar";
 import { UtilityDataProvider } from "@/components/providers/utility-data-provider";
 import { getUtilityData } from "@/data";
 import { db } from "@/lib/db";
-import React, { ReactNode } from "react";
+import React, { ReactNode, cache } from "react";
 
-// const fetchUtilityData = async () => {
-//   const baseurl = process.env.BASE_URL;
-//   const response = await fetch(baseurl + "api/utility-data", {
-//     cache: "force-cache",
-//   });
-//   const data = await response.json();
-//   return data;
-// };
+const fetchUtilityData = cache(async () => {
+  const baseurl = process.env.BASE_URL;
+  const response = await fetch(baseurl + "api/utility-data");
+  const data = await response.json();
+  return data;
+});
 
 const MainLayout = async ({ children }: { children: ReactNode }) => {
-  const data = await getUtilityData();
+  const data = await fetchUtilityData();
 
   return (
     <UtilityDataProvider {...data}>
